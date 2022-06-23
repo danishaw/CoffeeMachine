4be5d1d628242e9aee6a93037bd89bc9bdd5ba66
danishaw <danish20136@gmail.com>
2022-06-23 23:49:13 +0700

    export import

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-console.log("Menyalakan mesin kopi");
-console.log("Menggiling biji kopi");
-console.log("Memanaskan air");
-console.log("Mencampurkan air dan kopi");
-console.log("Menuangkan kopi ke dalam gelas");
-console.log("Menuangkan susu ke dalam gelas");
-console.log("Kopi Anda sudah siap!");
+const {coffeeStock, isCoffeeMachineReady} = require('./state')
+
+console.log(coffeeStock);
+
+const makeCoffee = (type, miligrams) => {
+    if (coffeeStock[type]>= miligrams) {
+        console.log("Kopi berhasil dibuat!");
+    } else {
+        console.log("Biji kopi habis!");
+    }
+}
+console.log(isCoffeeMachineReady);
+
+makeCoffee("robusta",80);
